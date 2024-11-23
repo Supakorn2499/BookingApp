c48f1e2f58da16227cd34bb83d16ee2c82823b3b
Supakorn-vTec <supakorn@vtec-system.com>
2024-11-23 21:28:18 +0700

    Update

---
+++ BookingApp.Server/Controllers/BankController.cs
@@ -0,0 +1,103 @@
+﻿using BookingApp.Server.Models;
+using BookingApp.Server.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingApp.Server.Controllers
+{
+    public class BankController : ControllerBase
+    {
+        private readonly BankService _bankService;
+
+        public BankController(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DatabaseConfig");
+            _bankService = new BankService(connectionString);
+        }
+
+
+        [HttpPost("Bank/Create")]
+        public async Task<IActionResult> CreateUser([FromBody] Bank? bank)
+        {
+            try
+            {
+                if (bank == null)
+                    return BadRequest();
+
+                var id = await _bankService.AddAsync(bank);
+                return CreatedAtAction(nameof(GetUserById), new { id }, bank);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+
+        }
+
+        [HttpDelete("Bank/Delete")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            try
+            {
+                var success = await _bankService.DeleteAsync(id);
+                return success ? StatusCode(200, "success") : NotFound();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+
+        }
+
+        [HttpPut("Bank/Update")]
+        public async Task<IActionResult> UpdateUser(int id, [FromBody] Bank? bank)
+        {
+            try
+            {
+                if (id != bank.id)
+                    return BadRequest();
+
+                var success = await _bankService.UpdateAsync(bank);
+                return success ? StatusCode(200, "success") : NotFound();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+
+        }
+
+        [HttpGet("Bank/Search")]
+        public async Task<IActionResult> Search(
+                    [FromQuery] string? keyword = null,
+                    [FromQuery] int pageNumber = 1,
+                    [FromQuery] int pageSize = 20)
+        {
+            if (pageNumber < 1 || pageSize < 1)
+                return BadRequest("Invalid pagination parameters.");
+
+            var result = await _bankService.GetAsync(keyword, pageNumber, pageSize);
+            var response = new
+            {
+                Data = result.Banks,
+                TotalRecords = result.TotalRecords,
+                CurrentPage = pageNumber,
+                TotalPages = (int)Math.Ceiling(result.TotalRecords / (double)pageSize)
+            };
+
+            return Ok(response);
+        }
+
+        [HttpGet("Bank/GetById")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var user = await _bankService.GetByIdAsync(id);
+
+            if (user == null)
+                return NotFound($"Bank with ID {id} not found.");
+
+            return Ok(user);
+        }
+
+
+    }
+}
---
+++ BookingApp.Server/Services/BankRepository.cs
@@ -0,0 +1,147 @@
+﻿using BookingApp.Server.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Npgsql;
+using System.ComponentModel.Design;
+using System;
+using Dapper;
+
+namespace BookingApp.Server.Services
+{
+    public class BankRepository
+    {
+        private readonly string _connectionString;
+
+        public BankRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<int> AddAsync(Bank bank)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                const string query = @"
+                INSERT INTO bank (companyid,code, name1,name2, active,inactivedate,createby, createatutc) 
+                VALUES (@companyid,@code, @name1,@name2, @active,@inactivedate,@createby, @createatutc)
+                RETURNING id;";
+
+                if (bank.active == "Y")
+                {
+                    bank.inactivedate = DateTimeHelper.ConvertToUtc(DateTime.Now);
+                }
+                var paraments = new
+                {
+                    companyid = bank.companyid,
+                    code = bank.code,
+                    name1 = bank.name1,
+                    name2 = bank.name2,
+                    active = bank.active,
+                    inactivedate = bank.inactivedate,
+                    createby = bank.createby,
+                    createatutc = DateTimeHelper.ConvertToUtc(DateTime.Now)
+                };
+
+                return await connection.ExecuteScalarAsync<int>(query, paraments);
+            }
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                const string query = "DELETE FROM bank WHERE id = @id;";
+                var rowsAffected = await connection.ExecuteAsync(query, new { id });
+                return rowsAffected > 0;
+            }
+        }
+
+        public async Task<bool> UpdateAsync(Bank bank)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                const string query = @"
+                UPDATE bank 
+                SET 
+                    companyid = @companyid,
+                    code= @code,
+                    name1 = @name1,
+                    name2 = @name2,
+                    active = @active,
+                    inactivedate = @inactivedate,
+                    updateby=@updateby, 
+                    updateatutc=@updateatutc
+                WHERE id = @id;";
+                if (bank.active == "Y")
+                {
+                    bank.inactivedate = DateTimeHelper.ConvertToUtc(DateTime.Now);
+                }
+                var paraments = new
+                {
+                    id = bank.id,
+                    companyid = bank.companyid,
+                    code = bank.code,
+                    name1 = bank.name1,
+                    name2 = bank.name2,
+                    active = bank.active,
+                    inactivedate = bank.inactivedate,
+                    updateby = bank.updateby,
+                    updateatutc = DateTimeHelper.ConvertToUtc(DateTime.Now)
+                };
+
+                var rowsAffected = await connection.ExecuteAsync(query, paraments);
+                return rowsAffected > 0;
+            }
+        }
+
+        public async Task<(IEnumerable<Bank> Banks, int TotalRecords)> GetAsync(string? keyword, int pageNumber, int pageSize)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                const string query = @"
+            SELECT * 
+            FROM bank
+            WHERE (@Keyword IS NULL OR 
+                   LOWER(name1) LIKE LOWER(@Keyword) OR 
+                   LOWER(name2) LIKE LOWER(@Keyword) OR 
+                   LOWER(code) LIKE LOWER(@Keyword))
+            ORDER BY id
+            OFFSET @Offset ROWS FETCH NEXT @PageSize ROWS ONLY;
+
+            SELECT COUNT(*)
+            FROM bank
+            WHERE (@Keyword IS NULL OR 
+                   LOWER(name1) LIKE LOWER(@Keyword) OR 
+                   LOWER(name2) LIKE LOWER(@Keyword) OR 
+                   LOWER(code) LIKE LOWER(@Keyword));";
+
+                var offset = (pageNumber - 1) * pageSize;
+
+                using (var multi = await connection.QueryMultipleAsync(query, new
+                {
+                    Keyword = string.IsNullOrWhiteSpace(keyword) ? null : $"%{keyword}%",
+                    Offset = offset,
+                    PageSize = pageSize
+                }))
+                {
+                    var banks = await multi.ReadAsync<Bank>();
+                    var totalRecords = await multi.ReadFirstAsync<int>();
+                    return (banks, totalRecords);
+                }
+            }
+        }
+
+        public async Task<Bank?> GetByIdAsync(int id)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                const string query = @"
+            SELECT * 
+            FROM bank
+            WHERE id = @id;";
+
+                return await connection.QueryFirstOrDefaultAsync<Bank>(query, new { id = id });
+            }
+        }
+
+    }
+}
